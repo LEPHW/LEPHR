c8894a951e11d8169cda9fd4f68bef4eb35f0e68
LEP <1569655707@qq.com>
2023-11-22 17:25:25 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '黑马人力资源后台管理项目',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
